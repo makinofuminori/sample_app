9662a5d184232485a82472943609b4d142e76129
牧野文則 <makino@me.com>
2014-05-20 14:55:14 +0900

    Initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -16,7 +16,12 @@ def secure_token
   token_file = Rails.root.join('.secret')
   if File.exist?(token_file)
     #Use the existing token.
-    File.read(token_file, token)
+    File.read(token_file)
+    token
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
     token
   end
 end
